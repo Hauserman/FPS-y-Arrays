a1392ba9bbc3a71fa5b454992998c8a320ff3e45
Tomas Hauser <142511066+Hauserman@users.noreply.github.com>
2024-06-06 12:10:45 -0300

    masomenos

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+
+    public int nivelHambre;
+    // Start is called before the first frame update
+    void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            TomarAlimento(alimento.valorAlimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
